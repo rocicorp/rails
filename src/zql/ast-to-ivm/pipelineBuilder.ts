caaad5f36b6d853fe45b3ae916c608197f5fe039
Matt <1009003+tantaman@users.noreply.github.com>
2024-03-07 12:38:26 -0500

    pare back the AST to complete the simplest case of IVM

--- src/zql/ast-to-ivm/pipelineBuilder.ts
+++ src/zql/ast-to-ivm/pipelineBuilder.ts
@@ -1,43 +1,114 @@
-// import {nullthrows} from '../error/InvariantViolation.js';
-// import {DifferenceStream} from '../ivm/graph/DifferenceStream.js';
-// import {AST} from '../query/ZqlAst.js';
+import {nullthrows} from '../error/InvariantViolation.js';
+import {DifferenceStream} from '../ivm/graph/DifferenceStream.js';
+import {AST, Condition, ConditionList, Operator} from '../query/ZqlAst.js';
 
-// export function buildPipeline(
-//   sourceStreamProvider: (sourceName: string) => DifferenceStream<unknown>,
-//   ast: AST,
-// ) {
-//   // filters first
-//   // maps second
-//   // order is a param to materialization
-//   // as well as limit? How does limit work in materialite again?
-//   let stream = sourceStreamProvider(
-//     nullthrows(ast.table, 'Table not specified in the AST'),
-//   );
+export function buildPipeline(
+  sourceStreamProvider: (sourceName: string) => DifferenceStream<unknown>,
+  ast: AST,
+) {
+  // filters first
+  // maps second
+  // order is a param to materialization
+  // as well as limit? How does limit work in materialite again?
+  let stream = sourceStreamProvider(
+    nullthrows(ast.table, 'Table not specified in the AST'),
+  );
 
-//   if (ast.where) {
-//     stream = applyWhere(stream, ast.where);
-//   }
+  if (ast.where) {
+    stream = applyWhere(stream, ast.where);
+  }
 
-//   // if order is same as underlying stream, we can skip the sort
-//   // and we can apply the limit.
-//   // If order is different, we materialize the entire thing and sort it.
-//   // Then apply the limit when returning slices of the materialized stream.
-// }
+  if (ast.select) {
+    if (ast.select === 'count') {
+      stream = stream.linearCount();
+    } else {
+      stream = applySelect(stream, ast.select);
+    }
+  }
 
-// function applyWhere(stream: DifferenceStream<unknown>, where: ConditionList) {
-//   for (let i = 0; i < where.length; i++) {
-//     const condition = where[i];
-//     if (condition === 'AND') {
-//       stream = stream.filter(() => true);
-//     }
-//   }
-//   return stream;
-// }
+  // Note: the stream is technically attached at this point.
+  // We could detach it until the user actually runs (or subscribes to) the statement as a tiny optimization.
+  return stream;
+}
 
-/*
-x = 1 AND y = 2 OR z = 3 OR ()
+function applySelect(stream: DifferenceStream<unknown>, select: string[]) {
+  return stream.map(x => {
+    const ret: Record<string, unknown> = {};
+    for (const field of select) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ret[field] = (x as any)[field];
+    }
 
-1. Or can bifurcate the stream and then re-combine it.
-2. ANDing a new ConditionList?
-  - 
-*/
+    return ret;
+  });
+}
+
+function applyWhere(stream: DifferenceStream<unknown>, where: ConditionList) {
+  let ret = stream;
+  /*
+  We'll handle `OR` and parentheticals like so:
+  OR: We'll create a new stream for the LHS and RHS of the OR then merge together.
+  Parentheticals: We'll create a new stream for the LHS and RHS of the operator involved in combining the parenthetical then merge together.
+  
+  Example:
+  (a = 1 AND b = 2) OR (c = 3 AND d = 4)
+  Becomes
+        s
+       / \
+     a=1 c=3
+     /     \
+     b=2   d=4
+      \    /
+        OR
+         |
+
+  So `ORs` cause a fork (two branches that need to be evaluated) and then that fork is combined.
+  */
+  for (let i = 0; i < where.length; i++) {
+    const condition = where[i];
+    if (condition === 'AND') {
+      continue;
+    }
+
+    ret = applyCondition(ret, condition);
+  }
+
+  return ret;
+}
+
+function applyCondition(
+  stream: DifferenceStream<unknown>,
+  condition: Condition,
+) {
+  const operator = getOperator(condition.op);
+  return stream.filter(x =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    operator((x as any)[condition.field], condition.value.value),
+  );
+}
+
+// We're well-typed in the query builder so once we're down here
+// we can assume that the operator is valid.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getOperator(op: Operator): (l: any, r: any) => boolean {
+  switch (op) {
+    case '=':
+      return (l, r) => l === r;
+    case '<':
+      return (l, r) => l < r;
+    case '>':
+      return (l, r) => l > r;
+    case '>=':
+      return (l, r) => l >= r;
+    case '<=':
+      return (l, r) => l <= r;
+    case 'IN':
+      return (l, r) => r.includes(l);
+    case 'LIKE':
+      return (l, r) => l.includes(r);
+    case 'ILIKE':
+      return (l, r) => l.toLowerCase().includes(r.toLocaleLowerCase());
+    default:
+      throw new Error(`Operator ${op} not supported`);
+  }
+}
--- src/zql/query/ZqlAst.ts
+++ src/zql/query/ZqlAst.ts
@@ -1,38 +1,48 @@
 // Going for a subset of the SQL `SELECT` grammar
 // https://www.sqlite.org/lang_select.html
 
+// TODO: the chosen operator needs to constrain the allowed values for the value
+// input to the query builder.
 export type Operator = '=' | '<' | '>' | '>=' | '<=' | 'IN' | 'LIKE' | 'ILIKE';
+
 export type Primitive = string | number | boolean | null | bigint;
-type Ref = `${string}.${string}`;
+// type Ref = `${string}.${string}`;
 export type AST = {
   readonly table?: string;
   readonly alias?: number;
   readonly select?: string[] | 'count';
-  readonly subSelects?: {
-    readonly alias: string;
-    readonly query: AST;
-  }[];
+  // readonly subSelects?: {
+  //   readonly alias: string;
+  //   readonly query: AST;
+  // }[];
   readonly where?: ConditionList;
-  readonly joins?: {
-    readonly table: string;
-    readonly as: string;
-    readonly on: ConditionList;
-  }[];
+  // readonly joins?: {
+  //   readonly table: string;
+  //   readonly as: string;
+  //   readonly on: ConditionList;
+  // }[];
   readonly limit?: number;
-  readonly groupBy?: string[];
+  // readonly groupBy?: string[];
   readonly orderBy?: [string[], 'asc' | 'desc'];
-  readonly after?: Primitive;
+  // readonly after?: Primitive;
 };
 
-type Conjunction = 'AND' | 'OR' | 'NOT'; //| 'EXISTS';
+type Conjunction = 'AND'; // | 'OR' | 'NOT' | 'EXISTS';
 export type ConditionList = (Conjunction | Condition)[];
-type Condition =
-  | ConditionList
-  | {
-      field: string;
-      op: Operator;
-      value: {
-        type: 'literal' | 'ref' | 'query';
-        value: Primitive | Ref | AST;
-      };
+export type Condition =
+  // | ConditionList
+  {
+    field: string;
+    op: Operator;
+    value: {
+      type: 'literal';
+      value: Primitive;
     };
+    //  | {
+    //   type: 'ref';
+    //   value: Ref;
+    // } | {
+    //   type: 'query';
+    //   value: AST;
+    // };
+  };
