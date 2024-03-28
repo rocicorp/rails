28a43d6f80af6273482cb13dd6b1955905eec339
Erik Arvidsson <erik.arvidsson@gmail.com>
2024-03-28 12:17:38 +0100

    Add filterIter

--- src/zql/util/iterables.ts
+++ src/zql/util/iterables.ts
@@ -7,3 +7,23 @@ export function* mapIter<T, U>(
     yield f(t, index++);
   }
 }
+
+export function filterIter<V, O extends V>(
+  iter: Iterable<V>,
+  p: (value: V, index: number) => value is O,
+): Iterable<O>;
+export function filterIter<V>(
+  iter: Iterable<V>,
+  p: (value: V, index: number) => boolean,
+): Iterable<V>;
+export function* filterIter<V>(
+  iter: Iterable<V>,
+  p: (value: V, index: number) => boolean,
+): Iterable<V> {
+  let index = 0;
+  for (const value of iter) {
+    if (p(value, index++)) {
+      yield value;
+    }
+  }
+}
