b05c27a9dee7bf348068ad6ecf86065ec4cb15c0
Erik Arvidsson <erik.arvidsson@gmail.com>
2024-04-02 17:26:07 +0200

    chore(zql): Simplify UnaryOperator
    Extending OperatorBase is more code than implementing the interface
    directly.

--- src/zql/ivm/graph/operators/unary-operator.ts
+++ src/zql/ivm/graph/operators/unary-operator.ts
@@ -1,25 +1,24 @@
-import {Version} from '../../types.js';
-import {OperatorBase} from './operator.js';
-import {DifferenceStream, Listener} from '../difference-stream.js';
 import {Multiset} from '../../multiset.js';
+import {Version} from '../../types.js';
+import {DifferenceStream, Listener} from '../difference-stream.js';
 import {Request} from '../message.js';
+import {Operator} from './operator.js';
 
 /**
  * Operator that only takes a single argument
  */
-export class UnaryOperator<
-  I extends object,
-  O extends object,
-> extends OperatorBase<O> {
+export class UnaryOperator<I extends object, O extends object>
+  implements Operator
+{
   readonly #listener: Listener<I>;
   readonly #input: DifferenceStream<I>;
+  readonly #output: DifferenceStream<O>;
 
   constructor(
     input: DifferenceStream<I>,
     output: DifferenceStream<O>,
     fn: (version: Version, data: Multiset<I>) => Multiset<O>,
   ) {
-    super([input], output);
     this.#listener = {
       newDifference: (version, data) => {
         output.newData(version, fn(version, data));
@@ -30,6 +29,11 @@ export class UnaryOperator<
     };
     input.addDownstream(this.#listener);
     this.#input = input;
+    this.#output = output;
+  }
+
+  commit(v: number): void {
+    this.#output.commit(v);
   }
 
   messageUpstream(message: Request): void {
