b0a470b60b389bb0a0c832050e3e6d4bb69dd219
Erik Arvidsson <erik.arvidsson@gmail.com>
2024-04-02 17:28:24 +0200

    chore(zql): Remove OperatorBase
    It is no longer used

--- src/zql/ivm/graph/operators/operator.ts
+++ src/zql/ivm/graph/operators/operator.ts
@@ -1,11 +1,10 @@
 import {Version} from '../../types.js';
-import {DifferenceStream} from '../difference-stream.js';
 import {Request} from '../message.js';
 
 export interface Operator {
   /**
    * Notify along the graph that the transaction
-   * has been comitted
+   * has been committed
    */
   commit(version: Version): void;
   messageUpstream(message: Request): void;
@@ -17,30 +16,4 @@ export class NoOp implements Operator {
   commit(_v: Version): void {}
   messageUpstream(): void {}
   destroy(): void {}
-}
-
-/**
- * A dataflow operator (node) that has many incoming edges (stream) and one outgoing edge (stream).
- */
-export abstract class OperatorBase<O extends object> implements Operator {
-  // upstream inputs
-  protected readonly _inputs: DifferenceStream<object>[];
-  // downstream output
-  protected readonly _output: DifferenceStream<O>;
-
-  constructor(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    inputs: DifferenceStream<any>[],
-    output: DifferenceStream<O>,
-  ) {
-    this._inputs = inputs;
-    this._output = output;
-  }
-
-  commit(v: Version) {
-    this._output.commit(v);
-  }
-
-  abstract messageUpstream(message: Request): void;
-  abstract destroy(): void;
 }
